da571ca078d97782bc21c58ee852cedba7bb05ed
Napoleongl <gustav.n.lundberg@outlook.com>
2020-12-01 22:40:03 +0100

    Replaced valkretskod wit valkretsnamn made vectors for mappings anf summary levels added map fixed waffle colours by sorting (why it doesnt honor colour names is a mystery)

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -2,21 +2,44 @@ library(dplyr)
 library(magrittr)
 library(readr)
 library(tidyr)
+library(patchwork)
+library(sf)
 
 source("R/functions.R")
+source("R/graphs.R")
 
 datapath = "../input/valresultat/"
+map_path <- "../input/sweden-maps/"
 year = 2018L
-valid_parties = c('C', 'KD', 'L', 'M', 'MP', 'S', 'SD', 'V')
-party_colours <- setNames(c('#009933', '#231977', '#6BB7EC', '#1B49DD', '#83CF39', '#EE2020', '#DDDD00', '#AF0000'), 
-                          valid_parties)
-valid_parties = c('V', 'MP', 'S', 'C', 'L', 'M', 'KD', 'SD')
-party_colours <- setNames(c('#AF0000','#83CF39', '#EE2020', '#009933', '#6BB7EC', '#1B49DD', '#231977',  '#EE2020'), 
-                          valid_parties)
-block_colours <- c(party_colours, "GAL" = "#22ccd8", "TAN" = "#010777", "Röd-Gröna" = "red", "Alliansen"  = "blue")
-
-summary_level = 'VALKRETSKOD'
 float_seats = 39L
+seat_rows = 18
+summary_level = summary_levels[3]
+summary_levels <- c("KOMMUNNAMN", "LÄNSNAMN", "VALKRETSNAMN")
+
+valid_parties = c('V', 'MP', 'S', 'C', 'L', 'M', 'KD', 'SD')
+party_colours <- setNames(c('#AF0000','#83CF39', '#EE2020', '#009933', 
+                            '#6BB7EC', '#1B49DD', '#231977',  '#DDDD00'),
+                          valid_parties)
+
+party_mapping <- tibble(party = valid_parties, 
+                        raw = valid_parties, 
+                        galtan = c(rep("GAL", 5), 
+                                   rep("TAN",3)),
+                        val2014 =c(rep("Röd-Gröna", 3),
+                                   rep("Alliansen", 4),
+                                   "SD")
+                        )
+block_colours <- list(raw     = party_colours,
+                      galtan  = c("GAL" = "#22ccd8", 
+                                  "TAN" = "#010777"),
+                      val2014 = c("Röd-Gröna" = "#EE2020", 
+                                  "Alliansen"  = "lightblue", 
+                                  party_colours["SD"])
+                      )
+
+available_mappings <- names(block_colours)
+selected_mapping = available_mappings[3]
+
 fixed_seats = read_csv(paste0(datapath, 'fasta_mandat_',summary_level, '_', year, '.csv'),
                           col_types = list(col_character(), col_integer()), 
                           progress = FALSE)
@@ -24,17 +47,23 @@ fixed_seats = read_csv(paste0(datapath, 'fasta_mandat_',summary_level, '_', year
 rawdata <- readxl::read_xlsx(path = paste0(datapath, year, '_R_per_valdistrikt.xlsx'),
                             sheet = 'R antal')
 
-districts  <- rawdata[, 1:8] %>% inner_join(fixed_seats, by = c('VALKRETSNAMN'='Valkrets'))
-votes      <- rawdata[, c('LÄNSKOD', 'KOMMUNKOD', 'VALKRETSKOD', 'VALDISTRIKTSKOD', valid_parties)] %>% 
+districts  <- rawdata[, 1:8] %>% inner_join(fixed_seats)
+votes      <- rawdata[, c('LÄNSNAMN', 'KOMMUNNAMN', 'VALKRETSNAMN', 'VALDISTRIKTSKOD', valid_parties)] %>% 
   pivot_longer(cols = all_of(valid_parties),names_to = "party", values_to = "votes")
 
-party_mapping <- tibble(party = valid_parties, 
-                        raw = valid_parties, 
-                        galtan = c(rep("GAL", 5), rep("TAN",3)),
-                        val2014 =c(rep("Röd-Gröna", 3), rep("Alliansen", 4), "SD")
-                        )
+swe_map_data <- read_sf(paste0(map_path, "gadm36_SWE_shp/", "gadm36_SWE_2L.shp")) %>% 
+  mutate(NAME_2 = replace(NAME_2, NAME_2=="Malung", "Malung-Sälen")) %>% 
+  mutate(NAME_2 = replace(NAME_2, NAME_2=="Upplands-Väsby", "Upplands Väsby")) %>% 
+  mutate(kommun_join = tolower(NAME_2)) %>% 
+  inner_join(districts %>% 
+               group_by(KOMMUNNAMN) %>% 
+               select(!!summary_levels) %>% 
+               unique() %>% 
+               mutate(kommun_join = tolower(KOMMUNNAMN))
+  ) 
 
-selected_mapping = 'val2014'
+
+
 party_to_block <- party_mapping %>%  
   select(party,  !!selected_mapping) %>% 
   rename(block = !!selected_mapping)
@@ -49,11 +78,11 @@ votes_by_level <- votes %>%
   summarise(wvotes = max(votes), block = block[which(votes == wvotes)]) # Winner per level after summary
 
 fixed_seats_assigned <- districts %>%  
-  group_by(VALKRETSKOD) %>% 
+  group_by(.dots=c(summary_level)) %>% 
   summarise(seats =  max(seats)) %>%
   inner_join(votes_by_level) %>% 
   group_by(block) %>%
-  summarise(fixed_seats = sum(seats))
+  summarise(seats = sum(seats))
 
 # Calculate 'utjämningsmandat' on national level
 votes_total <- votes %>% 
@@ -62,20 +91,29 @@ votes_total <- votes %>%
   group_by(block) %>% 
   summarise(votes = sum(votes, na.rm=TRUE) )
 
-float_seats_assigned <- sainte_lague_seat_dist(votes_total, float_seats)
+float_seats_assigned <- sainte_lague_seat_dist(votes_total, float_seats) %>% 
+  arrange(match(block, names(block_colours[[selected_mapping]]))) # Sorting necessary on data since waffle-plots don't look up colours by name.
 
 # Join float and fixed seats
 total_seats_assigned <- fixed_seats_assigned %>% 
-  full_join(float_seats_assigned, by = "block") %>% 
+  union(float_seats_assigned, by = "block") %>% 
   group_by(block) %>% 
-  summarize(seats = sum(fixed_seats, float_seats, na.rm = TRUE))
+  summarize(seats = sum(seats, na.rm = TRUE)) %>%
+  arrange(match(block, names(block_colours[[selected_mapping]])))
 
 total_seats <- sum(total_seats_assigned$seats)
 
-parliament_plot(total_seats_assigned, block_colours)
+votes_map_data <- swe_map_data %>% 
+  group_by(.dots=c(summary_level)) %>% 
+  summarise() %>% 
+  inner_join(votes_by_level)
 
-float_seat_plot(float_seats, block_colours)
+votes_map(votes_map_data, block_colours[[selected_mapping]]) +
+ 
+parliament_plot(total_seats_assigned, block_colours[[selected_mapping]], seat_rows) +
 
-# TODO:
-# Gruppera polygoner per summary level
-# Plotta Fördelnign av platser
+plot_layout(nrow = 1, ncol = 2)
+  
+# manual version of waffles, displays fontawesome but varies in width
+#parliament_plot2(fixed_seats_assigned, block_colours[[selected_mapping]], seat_rows) + theme(legend.position =  "none")+
+#parliament_plot2(float_seats_assigned, block_colours[[selected_mapping]], ceiling(seat_rows/3)) +
